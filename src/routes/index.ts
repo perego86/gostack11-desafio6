00f21759254a6075ced55b8684c3529c8c36a062
Daniel Perego <perego86@gmail.com>
2020-05-26 21:54:07 -0300

    added cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import cors from 'cors';
 
 import transactionsRouter from './transactions.routes';
 
 const routes = Router();
 
+routes.use(cors());
 routes.use('/transactions', transactionsRouter);
 
 export default routes;
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import { getCustomRepository, UpdateDateColumn } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 
 import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-import Transaction from '../models/Transaction';
 import DeleteTransactionService from '../services/DeleteTransactionService';
 import ImportTransactionsService from '../services/ImportTransactionsService';
 
@@ -16,10 +15,6 @@ const upload = multer(uploadConfig);
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
   const transactions = await transactionsRepository.find();
-
-  transactions.forEach((trans: Transaction) => {
-    delete trans.category_id;
-  });
 
   const balance = await transactionsRepository.getBalance();
 
